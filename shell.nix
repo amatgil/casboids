701d5178f403a55ab1b28c953da1212c784ca793
amatgil <amatgilvinyes@gmail.com>
2025-06-03 03:00:30 +0200

    R1 works

---
+++ shell.nix
@@ -0,0 +1,26 @@
+{
+  pkgs ? import <nixpkgs> { },
+  lib,
+}:
+let
+  packages = with pkgs; [
+    uiua-unstable
+
+    libGL
+    pkg-config
+    xorg.libXrandr
+    xorg.libXinerama
+    xorg.libXcursor
+    xorg.libXi
+    xorg.libX11
+  ];
+in
+pkgs.mkShell {
+  # Get dependencies from the main package
+  nativeBuildInputs = packages;
+  buildInputs = packages;
+  env = {
+    LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+    LD_LIBRARY_PATH = "${lib.makeLibraryPath packages}";
+  };
+}
